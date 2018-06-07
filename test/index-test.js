48c0ead6631a7818cc16cc04d038b593b77b5f6a
Drew Pham <drewvpham@gmail.com>
2018-06-07 17:16:40 -0500

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -46,16 +46,8 @@ describe('JavaScript Inheritance Patterns', function() {
       expect(Circle.prototype).toBeA(Shape)
       expect(Circle.prototype).toNotBeA(Polygon)
     })
-    it('knows its diameter and radius', function() {
-      var c = new Circle(2)
-      expect(c.diameter()).toBe(4)
-      expect(c.radius).toBe(2)
-    })
-    it('knows its area and circumference', function() {
-      var c = new Circle(2)
-      expect(c.area()).toBe(Math.PI * c.radius ** 2)
-      expect(c.circumference()).toBe(2 * Math.PI * c.radius)
-    })
+
+    
   })
 
   describe('Polygon', function() {
