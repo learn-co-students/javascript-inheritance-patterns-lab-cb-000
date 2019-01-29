d6afe139cfafcd5d6228164a17afcdb4c46add95
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-29 20:41:07 +0000

    Done.

--- index.js
+++ index.js
@@ -62,41 +62,41 @@ function Triangle(sideOneLength, sideTwoLength, sideThreeLength) {
   Polygon.call(this, [new Side(sideOneLength), new Side(sideTwoLength), new Side(sideThreeLength)])
 }
 
-// Triangle.prototype = Object.create(Polygon.prototype);
-// Triangle.prototype.constructor = Triangle;
-//
-// function Quadrilateral(sideOneLength, sideTwoLength, sideThreeLength, sideFourLength) {
-//   Polygon.call(this, [new Side(sideOneLength), new Side(sideTwoLength), new Side(sideThreeLength), new Side(sideFourLength)]);
-// }
-//
-// Quadrilateral.prototype = Object.create(Polygon.prototype);
-// Quadrilateral.prototype.constructor = Quadrilateral;
-//
-// function Rectangle(width, height) {
-//   Quadrilateral.call(this, width, height, width, height);
-//   this.width = width;
-//   this.height = height;
-// }
-// Rectangle.prototype = Object.create(Quadrilateral.prototype);
-// Rectangle.prototype.constructor = Rectangle
-// Rectangle.prototype.area = function() {
-//   return this.width * this.height;
-// }
-//
-// function Square(length) {
-//   Rectangle.call(this, length, length)
-//   this.length = length;
-// }
-//
-// Square.prototype = Object.create(Rectangle.prototype);
-// Square.prototype.constructor = Square
-//
-// Square.prototype.listProperties = function() {
-//   var props = "";
-//   for (var prop in this) {
-//     if(this.hasOwnProperty(prop)) {
-//       props += "this." + prop + " = " + this[prop] + "\n";
-//     }
-//   }
-//   return(props);
-// }
+Triangle.prototype = Object.create(Polygon.prototype);
+Triangle.prototype.constructor = Triangle;
+
+function Quadrilateral(sideOneLength, sideTwoLength, sideThreeLength, sideFourLength) {
+  Polygon.call(this, [new Side(sideOneLength), new Side(sideTwoLength), new Side(sideThreeLength), new Side(sideFourLength)]);
+}
+
+Quadrilateral.prototype = Object.create(Polygon.prototype);
+Quadrilateral.prototype.constructor = Quadrilateral;
+
+function Rectangle(width, height) {
+  Quadrilateral.call(this, width, height, width, height);
+  this.width = width;
+  this.height = height;
+}
+Rectangle.prototype = Object.create(Quadrilateral.prototype);
+Rectangle.prototype.constructor = Rectangle
+Rectangle.prototype.area = function() {
+  return this.width * this.height;
+}
+
+function Square(length) {
+  Rectangle.call(this, length, length)
+  this.length = length;
+}
+
+Square.prototype = Object.create(Rectangle.prototype);
+Square.prototype.constructor = Square
+
+Square.prototype.listProperties = function() {
+  var props = "";
+  for (var prop in this) {
+    if(this.hasOwnProperty(prop)) {
+      props += "this." + prop + " = " + this[prop] + "\n";
+    }
+  }
+  return(props);
+}
